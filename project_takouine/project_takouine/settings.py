9d16f27a108fab9073727940a5a722cd8c62d950
SmallingChris <mohamedguessoum120@gmail.com>
2024-07-27 12:55:47 +0100

    Push Updates 3

--- project_takouine/project_takouine/settings.py
+++ project_takouine/project_takouine/settings.py
@@ -82,12 +82,29 @@ WSGI_APPLICATION = 'project_takouine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_partner',
+        'USER': 'root',
+        'PASSWORD': 'Password24',
+        'HOST': '54.37.65.172',
+        'PORT': '3307',
     }
 }
+
 
 
 # Password validation
